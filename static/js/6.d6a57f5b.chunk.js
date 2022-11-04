198eed28fea1484fb6edaead923ce8a383d614e7
AIASolution <58142664+bestsolution4u@users.noreply.github.com>
2022-11-04 12:45:41 -0600

    change

--- static/js/6.d6a57f5b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-app"]=this["webpackJsonpreact-app"]||[]).push([[6],{335:function(p,n){},341:function(p,n){},343:function(p,n){}}]);
-//# sourceMappingURL=6.d6a57f5b.chunk.js.map
--- static/js/8.4600e977.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-app"]=this["webpackJsonpreact-app"]||[]).push([[8],{270:function(p,a){}}]);
-//# sourceMappingURL=8.4600e977.chunk.js.map
